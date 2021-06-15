aadbb388991dafe88aab5f9c2da20131979b7619
mbrown <tugtuj@gmail.com>
2021-06-14 23:02:50 -0400

    Query route, added ejs, layouts

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const addressList = require('./addresses.json')
+const cors = require('cors')
 // const dotenv = require('dotenv')
 
 // dotenv.config()
 
 const app = express()
+app.set('view engine', 'ejs')
+
+app.use(cors())
+app.use(express.static('/public'))
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Homepage.')
@@ -14,4 +19,30 @@ app.get('/addresses', (req, res) => {
     res.send(addressList)
 })
 
-app.listen(3000, () => { console.log(`Port is running on 3000`)})
+app.get('/addresses/:filterAddresses', (req, res) => {
+    let filteredAddresses = []
+    const filterAddressesFromSearch = () => {
+        for (let address of addressList) {
+            let addressValues = Object.values(address)
+            let filtered = addressValues.filter(address => address.includes(req.params.filterAddresses))
+
+            for (let addressLines of addressValues) {
+                if (addressLines == filtered) {
+                    filteredAddresses.push(address)
+                }
+            }
+
+        }
+    }
+    filterAddressesFromSearch()
+    res.send(filteredAddresses)
+})
+
+
+
+app.get('/home', (req, res) => {
+    res.render('index')
+})
+
+
+app.listen(8000, () => { console.log(`Port is running on 8000`)})
