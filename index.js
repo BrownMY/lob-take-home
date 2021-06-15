0235d20aa6c79059061461d36a698fa8ec1b6d3a
mbrown <tugtuj@gmail.com>
2021-06-15 13:20:06 -0400

    Deleting unneccessary files and dependencies

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express')
 const addressList = require('./addresses.json')
-const cors = require('cors')
 // const dotenv = require('dotenv')
 
 // dotenv.config()
 
 const app = express()
-app.set('view engine', 'ejs')
-
-app.use(cors())
-app.use(express.static('/public'))
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Homepage.')
@@ -19,12 +14,12 @@ app.get('/addresses', (req, res) => {
     res.send(addressList)
 })
 
-app.get('/addresses/:filterAddresses', (req, res) => {
+app.get(`/addresses/:filter`, (req, res) => {
     let filteredAddresses = []
     const filterAddressesFromSearch = () => {
         for (let address of addressList) {
             let addressValues = Object.values(address)
-            let filtered = addressValues.filter(address => address.includes(req.params.filterAddresses))
+            let filtered = addressValues.filter(address => address.includes(req.params.filter))
 
             for (let addressLines of addressValues) {
                 if (addressLines == filtered) {
@@ -36,12 +31,6 @@ app.get('/addresses/:filterAddresses', (req, res) => {
     }
     filterAddressesFromSearch()
     res.send(filteredAddresses)
-})
-
-
-
-app.get('/home', (req, res) => {
-    res.render('index')
 })
 
 
